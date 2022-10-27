cc22f258ef893ae7ab2ea3af81d4f4aa7e80e26d
Jimmi Robles Casanova <jimmirobles@icloud.com>
2022-10-27 16:30:09 -0500

    Laravel breeze scaffolding

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-    return redirect()->route('events');
+    return view('welcome');
 })->name('home');
 
-Route::get('/eventos', function () {
-    return view('events');
-})->name('events');
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+require __DIR__.'/auth.php';
