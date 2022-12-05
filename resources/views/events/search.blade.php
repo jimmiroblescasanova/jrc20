a9d3771086763e340ca16d422f10d457bdb0d419
Jimmi Robles <jimmirobles@icloud.com>
2022-12-05 15:47:57 -0500

    Implemented media library spatie

--- resources/views/events/search.blade.php
+++ resources/views/events/search.blade.php
@@ -16,7 +16,7 @@
                 <a href="{{ route('guest.events.show', $event) }}"
                     class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100">
                     <img class="object-cover rounded-t-lg max-h-48 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="{{ asset($event->image) }}" alt="{{ $event->title }}">
+                        src="{{ $event->getFirstMediaUrl('events', 'mini') }}" alt="{{ $event->title }}">
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                             {{ $event->title }}
